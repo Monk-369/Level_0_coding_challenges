db53d728a47e195632ab1a30253bd946366a4c5d
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-04-19 21:12:54 +0200

    Update Task05.py

--- Task05.py
+++ Task05.py
@@ -1,5 +1,5 @@
-def area_of_triangle(x, y, z):
-    s_p = 0.5 * (x + y + z)
-    area = (s_p * (s_p - x) * (s_p - y) * (s_p - z)) ** 0.5
+def area_of_triangle(num_1, num_2, num_3):
+    semiperimeter = 0.5 * (num_1 + num_2 + num_3)
+    area = (semiperimeter * (semiperimeter - num_1) * (semiperimeter - num_2) * (semiperimeter - num_3)) ** 0.5
     return area
 print(area_of_triangle(9, 13, 6))
