4c8e6f4a6fa243609af9e4960927a3a16fa5ff8c
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-04-19 15:09:00 +0200

    Update Task05.py

--- Task05.py
+++ Task05.py
@@ -2,4 +2,4 @@ def area_of_triangle(x, y, z):
     s_p = 0.5 * (x + y + z)
     area = (s_p * (s_p - x) * (s_p - y) * (s_p - z)) ** 0.5
     return area
-area_of_triangle('3', '4', '5')
+print(area_of_triangle(9, 13, 6))
