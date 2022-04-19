3079fec20ccf70fa177c0d9dbaaf53fc9a3c655e
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-04-19 14:37:14 +0200

    Update Task06.py

--- Task06.py
+++ Task06.py
@@ -5,11 +5,9 @@ def maximum(x, y, z):
         print(x)
     else:
         print(z)
-        
+maximum('12', '32', '6')
 #Bonus question
-
 def infinite_numbers(*numbers):
     for number in numbers:
         print(number)
-
-#infinite_numbers(2, 2, 3, 7, 8, 9, 29, 11, 910, 92, 260)
+infinite_numbers('2', '2', '3', '7', '8', '9', '29', '11', '910', '92', '260')
