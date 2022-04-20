65ab280ec71cc0afb847350dd8669b2bab9e8acf
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-04-20 10:31:39 +0200

    Update Task07.py

--- Task07.py
+++ Task07.py
@@ -1,10 +1,8 @@
 def convert(celsius):
-    temperature = celsius
     fahrenheit = (celsius * 1.8) + 32
     return fahrenheit
 print(convert(32), 'fahrenheit')
 def convert(fahrenheit):
-    temperature = fahrenheit
     celsius = (fahrenheit - 32) / 1.8
     return celsius
-print(convert(89.6), 'celsius'))
+print(convert(89.6), 'celsius')
