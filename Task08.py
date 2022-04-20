9821dae559b95242d9815dc4a871b3132fa85c42
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-04-20 10:59:03 +0200

    Update Task08.py

--- Task08.py
+++ Task08.py
@@ -1,7 +1,16 @@
-def convert_time(num):
+def convert_to_time(num):
     hours = num // 60
     minutes = num % 60
-    time = print(hours, 'hour(s)', minutes, 'minute(s)')
+    time = (hours, minutes)
+    if hours == 1 and minutes == 1:
+        print(hours, 'hour', minutes, 'minute')
+    elif hours == 1 and minutes > 1:
+        print(hours, 'hour', minutes, 'minutes')
+    elif hours > 1 and minutes == 1:
+        print(hours, 'hours', minutes, 'minute')
+    else:
+        print(hours, 'hours', minutes, 'minutes')
     return time
-convert_time(116)
+convert_to_time(116)
+
                 
