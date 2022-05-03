1c197c342def77b0107ccd204688dabc54e0afd3
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-05-03 09:49:33 +0200

    Update Task10.py

--- Task10.py
+++ Task10.py
@@ -1,6 +1,9 @@
 def common_character(string_1, string_2):
+    common_chars = []
+    string_1 = string_1.lower()
+    string_2 = string_2.lower()
     for letter in string_1:
         if letter in string_2:
-            print(letter.lower(), end = ' ')
-common_character('Bird', 'String')
-
+            common_chars.append(letter)
+    print(str(common_chars).lower())
+common_character('BiRd', 'String')
