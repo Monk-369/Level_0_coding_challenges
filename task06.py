bfd674c92c01e617897c85caccb0f47dd672374e
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-05-06 16:00:47 +0200

    included code for input of any number of numbers

--- task06.py
+++ task06.py
@@ -6,3 +6,11 @@ def maximum(num_1, num_2, num_3):
     else:
         print(num_3)
 maximum(12, 32, 6)
+def maximum(*list_1):
+    max_num = list_1[0]
+    for number in list_1:
+        if number > max_num:
+            max_num = number
+    return max_num
+print(maximum(12, 32, 6, 78, 90, 99, 877))
+
