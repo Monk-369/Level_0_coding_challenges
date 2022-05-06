cb8f254fde210ed984a025dc3aad36e05553d276
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-05-06 18:59:43 +0200

    updated code to consider all numbers as input

--- task08.py
+++ task08.py
@@ -8,9 +8,17 @@ def convert_to_time(num):
         print(hours, 'hour', minutes, 'minutes')
     elif hours > 1 and minutes == 1:
         print(hours, 'hours', minutes, 'minute')
+    elif hours == 0 and minutes == 1:
+        print(hours, 'hours', minutes, 'minute')
+    elif hours == 1:
+        print(hours, 'hour', minutes, 'minutes')
     else:
         print(hours, 'hours', minutes, 'minutes')
     return time
-convert_to_time(116)
+convert_to_time(133)
+convert_to_time(71)
+convert_to_time(60)
+convert_to_time(59)
+convert_to_time(1)
 
                 
