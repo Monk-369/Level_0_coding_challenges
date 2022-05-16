2aa714bde273ba6d9da11bc0eeb05d54f60feed9
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-05-16 15:19:03 +0200

    Function is now generalized for more than 3 numbers

--- task06.py
+++ task06.py
@@ -1,16 +1,8 @@
-def maximum(num_1, num_2, num_3):
-    if num_1 <= num_2 >= num_3:
-        print(num_2)
-    elif num_2 <= num_1 >= num_3:
-        print(num_1)
-    else:
-        print(num_3)
-maximum(12, 32, 6)
-def maximum(*list_1):
+def maximum_number(*list_1):
     max_num = list_1[0]
     for number in list_1:
         if number > max_num:
             max_num = number
     return max_num
-print(maximum(12, 32, 6, 78, 90, 99, 877))
+print(maximum_number(12, 32, 6, 78, 90, 99, 877))
 
