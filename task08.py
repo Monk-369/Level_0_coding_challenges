3d413e9b8552099b43559d8ef585aacd5909c437
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-05-16 15:25:24 +0200

    hours and minutes are now separated by a comma

--- task08.py
+++ task08.py
@@ -5,15 +5,15 @@ def convert_to_time(num):
     if hours == 1 and minutes == 1:
         print(hours, 'hour', minutes, 'minute')
     elif hours == 1 and minutes > 1:
-        print(hours, 'hour', minutes, 'minutes')
+        print(hours, 'hour,', minutes, 'minutes')
     elif hours > 1 and minutes == 1:
-        print(hours, 'hours', minutes, 'minute')
+        print(hours, 'hours,', minutes, 'minute')
     elif hours == 0 and minutes == 1:
-        print(hours, 'hours', minutes, 'minute')
+        print(hours, 'hours,', minutes, 'minute')
     elif hours == 1 and minutes != 1:
-        print(hours, 'hour', minutes, 'minutes')
+        print(hours, 'hour,', minutes, 'minutes')
     else:
-        print(hours, 'hours', minutes, 'minutes')
+        print(hours, 'hours,', minutes, 'minutes')
     return time
 convert_to_time(133)
 convert_to_time(71)
