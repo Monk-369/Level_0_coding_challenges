addcc57bfbea3a6f065514d6b55bde1570150ac6
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-06-13 16:43:18 +0200

    Made output bit more readable

--- task07.py
+++ task07.py
@@ -1,8 +1,8 @@
 def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * 1.8) + 32
     return fahrenheit
-print(convert_to_fahrenheit(32), 'fahrenheit')
+print(convert_to_fahrenheit(32), 'fahrenheit, is equal to 32 celsius')
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) / 1.8
     return celsius
-print(convert_to_celsius(89.6), 'celsius')
+print(convert_to_celsius(89.6), 'celsius, is equal to 89.6 fahrenheit')
