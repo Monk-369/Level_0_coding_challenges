9f3d6264e22464218d15ab2a87023e1f6366c3ec
Monk-369 <102608899+Monk-369@users.noreply.github.com>
2022-06-14 06:11:25 +0200

    Modified code per insruction given

--- task10.py
+++ task10.py
@@ -5,6 +5,6 @@ def common_character(string_1, string_2):
     for letter in string_1:
         if letter in string_2:
             common_chars.append(letter)
-            new_list = list(dict.fromkeys(common_chars))
+            new_list = list(set(common_chars))
     print('Common letters:', ', '.join(new_list))
 common_character('BiiRd', 'briing')
